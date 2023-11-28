230293275db1e89ad3fbc26b35c3ba693c379bf6
cgomezlop95 <c.gomezlop95@gmail.com>
2023-11-28 13:51:07 +0100

    all books and all sales done

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const prisma = require("../prisma"); 
 
-router.get('/', (req, res) => {})
+router.get("/", async (req, res) => {
+  try {
+    const books = await prisma.Libro.findMany();
+    res.json(books);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error", details: error.message });
+  }
+});
 
 module.exports = router;
+
+
--- routes/sales.js
+++ routes/sales.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const prisma = require("../prisma");
 
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+    try {
+      const sales = await prisma.Venta.findMany();
+      res.json(sales);
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ error: "Internal Server Error", details: error.message });
+    }
+  });
 
 module.exports = router;

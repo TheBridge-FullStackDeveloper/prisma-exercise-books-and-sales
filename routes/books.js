4152989a41dc905fe1a38aad7aa5a317ae6711ee
cgomezlop95 <c.gomezlop95@gmail.com>
2023-11-28 17:11:50 +0100

    queries done apart from joins

--- routes/books.js
+++ routes/books.js
@@ -1,17 +1,69 @@
 const express = require("express");
 const router = express.Router();
-const prisma = require("../prisma"); 
+const prisma = require("../prisma");
+
+//Endpoint /books: Muestra todos los libros.
 
 router.get("/", async (req, res) => {
   try {
     const books = await prisma.Libro.findMany();
     res.json(books);
   } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ error: "Internal Server Error", details: error.message });
+    console.error(error);
+    res.json("Server error");
   }
 });
 
+//Endpoint /books/:isbn: Muestra un libro en particular.
+
+router.get("/:isbn", async (req, res) => {
+  try {
+    const book = await prisma.Libro.findUnique({
+      where: {
+        ISBN: req.params.isbn,
+      },
+    });
+    res.json(book);
+  } catch (error) {
+    console.error(error);
+    res.json("Server error");
+  }
+});
+
+//Endpoint /books/author/:author: Muestra todos los libros de un autor en particular.
+
+router.get("/author/:author", async (req, res) => {
+  try {
+    const bookByAuthor = await prisma.Libro.findMany({
+      where: {
+        Autor: req.params.author,
+      },
+    });
+    res.json(bookByAuthor);
+  } catch (error) {
+    console.error(error);
+    res.json("Server error");
+  }
+});
+
+//Endpoint /books/price/:price: Muestra todos los libros que cuestan más de 20.
+
+router.get("/price/:price", async (req, res) => {
+  try {
+    const bookByPrice = await prisma.Libro.findMany({
+      where: {
+        Precio: {
+          gt: parseFloat(req.params.price),
+        },
+      },
+    });
+    res.json(bookByPrice);
+  } catch (error) {
+    console.error(error);
+    res.json("Server error");
+  }
+});
+
+//Endpoint /books/with-sales: Realiza un JOIN para combinar las tablas Libros y Ventas basándote en el ISBN.
+
 module.exports = router;
-
-
--- routes/sales.js
+++ routes/sales.js
@@ -2,14 +2,69 @@ const express = require("express");
 const router = express.Router();
 const prisma = require("../prisma");
 
+//Endpoint /sales: Muestra todas las ventas.
+
 router.get("/", async (req, res) => {
-    try {
-      const sales = await prisma.Venta.findMany();
-      res.json(sales);
-    } catch (error) {
-      console.error("Error:", error);
-      res.status(500).json({ error: "Internal Server Error", details: error.message });
-    }
-  });
+  try {
+    const sales = await prisma.Venta.findMany();
+    res.json(sales);
+  } catch (error) {
+    console.error("Error:", error);
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", details: error.message });
+  }
+});
+
+//Endpoint /sales/:id: Muestra una venta en particular.
+
+router.get("/:id", async (req, res) => {
+  console.log(req.params.id);
+  try {
+    const sale = await prisma.Venta.findUnique({
+      where: {
+        ID_Venta: parseInt(req.params.id),
+      },
+    });
+    res.json(sale);
+  } catch (error) {
+    console.error(error);
+    res.json("Server error");
+  }
+});
+
+//Endpoint /sales/book/:isbn: Muestra todas las ventas de un libro en particular.
+
+router.get("/book/:isbn", async (req, res) => {
+  try {
+    const salesByBook = await prisma.Venta.findMany({
+      where: {
+        ISBN: req.params.isbn,
+      },
+    });
+    res.json(salesByBook);
+  } catch (error) {
+    console.error(error);
+    res.json("Server error");
+  }
+});
+
+//Endpoint /sales/date/:date: Muestra todas las ventas realizadas en una fecha en particular. consejo: usa new Date() para convertir una cadena de fecha en un objeto de fecha y poder compararla con la fecha de la base de datos.
+
+router.get("/date/:date", async (req, res) => {
+  try {
+    const salesByDate = await prisma.Venta.findMany({
+      where: {
+        Fecha_Venta: new Date(req.params.date).toISOString(),
+      },
+    });
+    res.json(salesByDate);
+  } catch (error) {
+    console.error(error);
+    res.json("Server error");
+  }
+});
+
+//Endpoint /sales/top: Extrae el libro con el mayor ingreso total generado.
 
 module.exports = router;

202f52dda8eb679f0f27535fe593d02be6faf948
Adrián Mijarra <adrian.mijarra@gmail.com>
2023-12-03 21:33:04 +0100

    done ?

--- routes/books.js
+++ routes/books.js
@@ -1,18 +1,66 @@
 const express = require('express');
 const router = express.Router();
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
-router.get('/', (req, res) => {})
+// Endpoint /books
+router.get('/books', async (req, res) => {
+  try {
+    const books = await prisma.libro.findMany();
+    res.json({ books });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Error interno del servidor');
+  }
+});
 
-module.exports = router;
+// Endpoint /books/:isbn
+router.get('/books/:isbn', async (req, res) => {
+  const isbn = req.params.isbn;
+  try {
+    const book = await prisma.libro.findUnique({
+      where: { ISBN: isbn },
+    });
+    res.json({ book });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Error interno del servidor');
+  }
+});
 
-//- [ ] Endpoint `/books`:
-
-router.get("/", async (req, res) => {
+  //- [ ] Endpoint `/books/author/:author`:
+  router.get("/books/author/:author", async (req, res) => {
     try {
-      const books = await prisma.Libro.findMany();
-      res.json(books);
+      const decodedAuthor = decodeURIComponent(req.params.author);
+      const booksByAuthor = await prisma.libro.findMany({
+        where: {
+          Autor: decodedAuthor,
+        },
+      });
+      res.json(booksByAuthor);
     } catch (error) {
-      console.error(error);
       res.json("Server error");
     }
   });
+
+  // [ ] Endpoint `/books/price/:price`
+  
+  router.get("/books/price/:price", async (req, res) => {
+    try {
+      const requestedPrice = parseFloat(req.params.price);
+      const booksPrice = await prisma.libro.findMany({
+        where: {
+          Precio: {
+            gt: requestedPrice,
+          },
+        },
+      });
+  
+      res.json(booksPrice);
+    } catch (error) {
+
+      res.json("Server error");
+    }
+  });
+  
+  module.exports = router;
--- routes/sales.js
+++ routes/sales.js
@@ -7,12 +7,64 @@ module.exports = router;
 
 //- [ ] Endpoint `/sales`:
 
-router.get("/", async (req, res) => {
-    try {
-      const sales = await prisma.Venta.findMany();
-      res.json(sales);
-    } catch (error) {
+router.get("/sales", async (req, res) => {
+  try {
+    const sales = await prisma.venta.findMany();
+    res.json(sales);
+  } catch (error) {
       console.error(error);
       res.json("Server error");
     }
   });
+
+ // [ ] Endpoint `/sales/:id`
+ router.get("/sales/:id", async (req, res) => {
+  try {
+    const sale = await prisma.venta.findUnique({
+      where: {
+        ID_Venta: parseInt(req.params.id),
+      },
+    });
+    res.json(sale);
+  } catch (error) {
+    console.error(error);
+    res.json("Server error");
+  }
+});
+
+// - [ ] Endpoint `/sales/book/:isbn`:
+router.get("/sales/book/:isbn", async (req, res) => {
+  try {
+    const salesForBook = await prisma.venta.findMany({
+      where: {
+        ISBN_libro: req.params.isbn,
+      },
+    });
+    res.json(salesForBook);
+  } catch (error) {
+    console.error(error);
+    res.json("Server error");
+  }
+});
+
+//- [ ] Endpoint `/sales/date/:date`:
+
+router.get("/sales/date/:date", async (req, res) => {
+  try {
+    const requestedDate = new Date(req.params.date);
+    if (isNaN(requestedDate.getTime())) {
+      return res.status(400).json({ error: "Fecha inválida" });
+    }
+    
+    const salesForDate = await prisma.venta.findMany({
+      where: {
+        FechaVenta: requestedDate,
+      },
+    });
+
+    res.json(salesForDate);
+  } catch (error) {
+    console.error(error);
+    res.json("Server error");
+  }
+});

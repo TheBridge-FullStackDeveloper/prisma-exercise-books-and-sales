33b0e4fe5e28eb2602b43375f014e107d03f4975
Miguel Núñez Galán <miguelnunezgalan@Air-de-Miguel.technicolor.net>
2023-12-06 20:18:28 +0100

    completed

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,76 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { PrismaClient } = require("@prisma/client");
 
-router.get('/', (req, res) => {})
+const Prisma = new PrismaClient();
 
+router.get("/", async (req, res) => {
+  try {
+    const allBooks = await Prisma.libro.findMany({});
+    res.json(allBooks);
+  } catch (error) {
+    res.json("Internal Server Error");
+  }
+});
+
+router.get("/author/:author", async (req, res) => {
+  try {
+    const autor = await Prisma.libro.findMany({
+      where: {
+        Autor: req.params.author,
+      },
+    });
+    res.json(autor);
+  } catch (error) {
+    res.json("Internal Server Error");
+  }
+});
+
+router.get("/price/:price", async (req, res) => {
+  try {
+    const price = await Prisma.libro.findMany({
+      where: {
+        Precio: {
+          gt: Number(req.params.price),
+        },
+      },
+    });
+    res.json(price);
+  } catch (error) {
+    res.json("Internal Server Error");
+  }
+});
+
+router.get("/with-sales", async (req, res) => {
+  try {
+    const withSales = await Prisma.venta.findMany({
+      include: {
+        ibro: {
+          select: {
+            Titulo: true,
+            Autor: true,
+          },
+        },
+      },
+    });
+
+    res.json(withSales);
+  } catch (error) {
+    console.log(error);
+    res.json("Internal Server Error");
+  }
+});
+
+router.get("/:isbn", async (req, res) => {
+  try {
+    const isbn = await Prisma.libro.findUnique({
+      where: {
+        ISBN: req.params.isbn,
+      },
+    });
+    res.json(isbn);
+  } catch (error) {
+    res.json("Internal Server Error");
+  }
+});
 module.exports = router;
--- routes/sales.js
+++ routes/sales.js
@@ -1,6 +1,94 @@
 const express = require("express");
 const router = express.Router();
+const { PrismaClient } = require('@prisma/client');
 
-router.get("/", async (req, res) => {});
+const Prisma = new PrismaClient();
+
+router.get('/', async (req, res) => {
+    try {
+        const allSales = await Prisma.venta.findMany({});
+        res.json(allSales);
+    } catch (error) {
+        res.json( "Internal Server Error");
+    }
+});
+//- [ ] Endpoint `/sales/top`: Extrae el libro con el mayor ingreso total generado.
+router.get('/top', async (req, res) => {
+    try {
+        const topSales = await Prisma.venta.findMany({
+            select:{
+                Cantidad:true,
+                Libro:{
+                    select:{
+                        Titulo:true,
+                        Autor:true,
+                        Precio:true,
+                    }
+                }
+            }
+        });
+        const topSeller = topSales.reduce((acc,el)=>{
+            const multiply = el.Cantidad * el.Libro.Precio;
+            if (multiply > acc.multiply){
+                return{Libro:el.Libro.Titulo, multiply}
+            }
+            return acc;
+        },
+        {
+            Libro:null, 
+            multiply:0
+        })
+        res.json(topSeller);
+    } catch (error) {
+        res.json(error);
+    }
+});
+
+
+
+router.get('/book/:isbn', async (req, res) => {
+    try {
+        const isbn = await Prisma.venta.findMany({
+            where:{
+                ISBN : req.params.isbn
+            }
+        });
+        res.json(isbn);
+    } catch (error) {
+        console.log(error)
+        res.json(error);
+    }
+});
+
+router.get('/date/:date', async (req, res) => {
+    try {
+      const salesOnDate = await Prisma.venta.findMany({
+        where: {
+          Fecha_Venta: new Date(req.params.date)
+           
+        },
+      });
+  
+      res.json({ sales: salesOnDate });
+    } catch (error) {
+      res.json(error);
+    }
+  });
+  
+
+
+router.get('/:id', async (req, res) => {
+    try {
+        const idSales = await Prisma.venta.findUnique({
+            where:{
+                ID_Venta : Number(req.params.id)
+            }
+        });
+        console.log(idSales)
+        res.json(idSales);
+    } catch (error) {
+        res.json( "Internal Server Error");
+    }
+});
 
 module.exports = router;
